968c4d407b9ddacc89084705345ec8ffc34d5679
Parth-2112 <raghuwanshiparth@gmail.com>
2022-04-26 02:41:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var superh = require("superheroes");
+
+var nameh = superh.random();
+console.log(nameh);
+
+var superv = require("supervillains");
+
+var namev = superv.random();
+console.log(namev);
